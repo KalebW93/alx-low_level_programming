a9bbb27baa38b4e3f504f60ba9e7a2a24cc0fa47
Kaleb <wondwossenkale444@gmail.com>
2023-02-27 11:36:21 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - return the size of the string
+ * @s the string passd to the function
+ */
+int _strlen(char *s) /*_strlen - return the size of the string*/
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
