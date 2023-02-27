2525386162371a5d9cfc0a9a3079b8ff3a1f7826
Kaleb <wondwossenkale444@gmail.com>
2023-02-27 12:04:05 -0800

    comment4

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,8 +3,9 @@
  * _strlen - return the size of the string
  * @s: the string passd to the function
  *return the size of the string
+ *_strlen return 16
  */
-int _strlen(char *s)/* _strlen  return the size of s or the  string */
+int _strlen(char *s)/* _strlen  return the size of s or the  string 16 or a*/
 {
 	int a = 0;
 
