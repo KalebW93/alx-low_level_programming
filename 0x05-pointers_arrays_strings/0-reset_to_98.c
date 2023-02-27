b655c4f9ef818ee72c4d55d5153aa419291f43af
Kaleb <wondwossenkale444@gmail.com>
2023-02-27 10:56:55 -0800

    still with betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 /**
- * reset_to_98 - takes the pointer and updates the value 
- * owned By Kaleb\Wondwossen
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check 
+ * owned By Kaleb/Wondwossen
  * Return: 0 is success
  */
-void reset_to_98(int *n) /* n is uses parameter of the function8*/
+void reset_to_98(int *n) /* n is poiter which use*/
 {
 	*n = 98;
 }
