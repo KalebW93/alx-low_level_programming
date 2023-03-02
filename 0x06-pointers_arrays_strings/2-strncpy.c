9d255a39195417e1fa71b67666a6e0db959f18dc
Kaleb <wondwossenkale444@gmail.com>
2023-03-02 06:30:37 -0800

    string copy function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,21 +3,29 @@
 /**
  * _strncpy - copy string on string
  *
+ * @dest:char the destination of string to be copied
+ * @src:char the source of string to be copied
+ * @n: int the number of bytes to be copied
+ *
+ * Return: the pointer to the sting dest
  */
-char *_strcpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	char *s;
 
 	s = dest;
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n && *src; i++)
 	{
-		if(*src != '\0')
+		if (*src != '\0')
 			*dest = *src;
-		else if (*src == '\0')
-			*dest = '\0';
 		dest++;
 		src++;
+	}
+	for (; i < n; i++)
+	{
+		*dest = '\0';
+		dest++;
 	}
 	return (s);
 }
