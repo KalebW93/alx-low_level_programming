423ea6e3e1089a56c516cedbee9644236f60531f
Kaleb <wondwossenkale444@gmail.com>
2023-03-06 06:59:06 -0800

    function to return the searched postion using pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchar - this fucntion returns the first occuranc of the
+ *  character in variable c
+ *  @s: the string to be checked
+ *
+ *  @c: the character to be searched in the string
+ *
+ *  Return: reterurn to the memory location s or Null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
