494249d069330754683fdf7f619d0c7e01f47af6
Kaleb <wondwossenkale444@gmail.com>
2023-03-08 01:07:47 -0800

    printing text using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - this print the text using recursion
+ *
+ * @s: this contains the text to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	while (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
