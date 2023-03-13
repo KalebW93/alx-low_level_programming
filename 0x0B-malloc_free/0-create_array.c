68f896b74b2c5b546b48b7feff586f89ddc08a7a
Kaleb <wondwossenkale444@gmail.com>
2023-03-13 06:01:45 -0700

    fixing malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * create_array - crate dynamic array with the specfic size
@@ -12,13 +13,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 	ptr = malloc(sizeof(c) * size);
+	if  (ptr == NULL)
+	{
+		printf("Can't alloate %ld bytes (after %d calls)\n", sizeof(c) * size, i);
+			return (ptr);
+	}
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
