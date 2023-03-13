bc584fc928418f9163e4b74fcc8ee0bcb9e2c08d
Kaleb <wondwossenkale444@gmail.com>
2023-03-13 10:21:26 -0700

    some six

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,20 +15,25 @@ char *str_concat(char *s1, char *s2)
 	int len = 0, i = 0;
 
 	s3 = malloc(sizeof(s1) + sizeof(s2) - 1);
-	if (s1 == NULL || s2 == NULL)
+	if (s3 == NULL)
 	{
 		return (NULL);
 	}
-	while (s1[len] != '\0')
+	if (s1 != NULL)
 	{
-		s3[len] = s1[len];
-		len++;
-
+		while (s1[len] != '\0')
+		{
+			s3[len] = s1[len];
+			len++;
+		}
 	}
-	while (s2[i] != '\0')
+	if (s2 != NULL)
 	{
-		s3[len + i] = s2[i];
-		i++;
+		while (s2[i] != '\0')
+		{
+			s3[len + i] = s2[i];
+			i++;
+		}
 	}
 	s3[len + i + 1] = '\0';
 	return (s3);
