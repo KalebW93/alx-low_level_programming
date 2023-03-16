c4bbfdd7ad64b17a0ba8c2f58acf37cde5c4e3f2
Kaleb <wondwossenkale444@gmail.com>
2023-03-16 05:00:31 -0700

    betty style

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 /**
- * malloc_cheked - this allocate and check the allocation
+ * malloc_checked - this allocate and check the allocation
  * @b: size of memory to be allocated
  *
  * Return: the address if it iss succesfulor 98 for termination
@@ -11,9 +11,9 @@
 void *malloc_checked(unsigned int b)
 {
 	void *i;
-	
+
 	i = malloc(b);
-	if(i == NULL)
+	if (i == NULL)
 		exit(98);
 	return (i);
 }
