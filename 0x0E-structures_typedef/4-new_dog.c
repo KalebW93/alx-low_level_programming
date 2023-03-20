13ade34a2c8755d2302e59786888cb4e25b316de
Kaleb <wondwossenkale444@gmail.com>
2023-03-20 14:14:06 -0700

    assigneig the sturuct dynamicly

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creats and assign the new struct
+ * @name: name of the new code
+ * @age: age of hte new dog
+ * @owner: owner of the new dog
+ * Return: NULL ifthe function fails or the ponter to the new struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j, k;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		;
+	i++;
+	dog->name = malloc(i * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (j = 0; owner[j]; j++)
+		;
+	j++;
+	dog->owner = malloc(j * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k < j; k++)
+		dog->owner[k] = owner[k];
+	return (dog);
+}
