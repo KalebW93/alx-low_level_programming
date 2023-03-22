4cc4d86d2f45be41d1c518b087c1f648cd74cc0d
Kaleb <wondwossenkale444@gmail.com>
2023-03-22 08:48:32 -0700

    comarsion the array elemnts

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - is a function that searhes for an integer
+ * @array: array of element to be compared
+ * @size: size of the array
+ * @cmp: points to the function the at comare element
+ * Return: -1 ,index,
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, r = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			r = cmp(array[i]);
+			if (r != 0)
+				return (i);
+		}
+
+	}
+	return (-1);
+}
